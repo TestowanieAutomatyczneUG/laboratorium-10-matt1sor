955e004509243775a5b9677f3ead4e6e80036890
matt1sor <mattidepka@gmail.com>
2021-12-22 08:25:11 +0100

    Poprawione zad1

--- zad1/src/test.py
+++ zad1/src/test.py
@@ -1,5 +1,5 @@
 import unittest
-from src.Note import Note
+from Note import Note
 from assertpy import assert_that
 
 
@@ -9,15 +9,15 @@ class NoteTest(unittest.TestCase):
         assert_that(note.get_name()).is_equal_to("spr")
 
     def test_get_Note(self):
-        note = Note("spr", 4)
+        note = Note("test", 4)
         assert_that(note.get_note()).is_equal_to(4)
 
     def test_get_Note_Lower(self):
-        note = Note("spr", 2)
+        note = Note("test", 2)
         assert_that(note.get_note()).is_equal_to(2)
 
     def test_get_Note_Upper(self):
-        note = Note("spr", 6)
+        note = Note("test", 6)
         assert_that(note.get_note()).is_equal_to(6)
     def test_get_Name_empty(self):
         self.assertRaises(Exception, Note, "", 3)
@@ -26,7 +26,7 @@ class NoteTest(unittest.TestCase):
         self.assertRaises(Exception, Note, None, 3)
 
     def test_get_Note_too_low(self):
-        self.assertRaises(Exception, Note, "testowanie", 1)
+        self.assertRaises(Exception, Note, "Testowanie", 0)
 
     def test_get_Note_too_high(self):
-        self.assertRaises(Exception, Note, "testowanie", 7)
+        self.assertRaises(Exception, Note, "Testowanie", 9)
